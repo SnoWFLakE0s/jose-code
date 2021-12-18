922f8286b59cbea59d702b0cea9ffee2829d58ae
Matviy Zhachek <mzhachek3255@bths.edu>
2021-12-17 21:29:18 -0500

    added comments

--- src/main.c
+++ src/main.c
@@ -3,16 +3,15 @@
 #include "main.h"
 
 int main(void){
-    char fileName[80];
-    char inputName[80] = "../input/";
+    //Josh
+    char fileName[80]; //makes array to write User's filename to
+    char inputName[80] = "../input/"; //array for concatenation
     printf("Welcome to the %s compiler!\n",APP_NAME);
     printf("Please enter the name of your josecode program in the input directory: ");
     scanf("%s",&fileName[0]);
-    strcat(inputName,fileName);
+    strcat(inputName,fileName); //concatenates User's file name to the directory
 
-    //need to add function to wipe any output.c s
-    //need to add custom file name support
-
+    //Matt
     FILE *fp; //file pointer to input file
     fp = fopen(inputName,"r"); //opens input.josecode to be read
     if(fp == NULL) { //makes sure the file can be read
@@ -26,6 +25,7 @@ int main(void){
         return(-1);
     }
 
+    //Matt
     char input[80]; //input array 80 chars long
     while (!feof(fp)) { //reads the file to check there's no EOF
         if( fgets (input, 80, fp)!=NULL ) {
@@ -42,9 +42,10 @@ int main(void){
             }
         }
     }
-    fclose(op); 
-    printf("intermediary file successfully created\n");
+    fclose(op);// closes to then reopen to be read instead of written
+    printf("intermediary file successfully created\n");//success message
 
+    //Matt
     FILE *cp; //file pointer to output c file
     cp = fopen("../output/output.c","w"); //opens the output.c file for writing 
     if(cp == NULL) { //makes sure the file can be read
@@ -57,50 +58,47 @@ int main(void){
         return(-1);
     }
 
-    char *output_start[] = {
+    //Josh
+    char *output_start[] = {//start of an outline of an average C program
         "#include \"josecode.h\"",
         "\n",
         "int main(void) {",
         ""
     }, **i;
     i = output_start;
-    char *output_end[] = {
+    char *output_end[] = {//end of an outline of an average C program
         "return 0;\n",
         "}",
         ""
     }, **j;
     j = output_end;
-
-    while(*i != "") {
-        fprintf(cp, "%s\n", *i++);
+    while(*i != "") {//until the "" is detected
+        fprintf(cp, "%s\n", *i++);//writes to start of file
     }
 
-    char *line = NULL;
+    //combination of Matt and Josh
+    char *line = NULL;//setup for getline function
     size_t len = 0;
-    char space[] = " ";
-    char endpar[] = ");\n";
-    //int line_num = 1;
-    while(getline(&line, &len, op) != -1) {
+    while(getline(&line, &len, op) != -1) {//until eof is detected in getline function
         if(strcmp("(\n", line) == 0) { //if an open parenthese is detected
             char str_temp[80] = "( "; //create temporary string
             while(strcmp(line, ");\n") != 0) { //until close parenthese is detected
-                //printf("input line =%s", line);
                 getline(&line, &len, op); //get a new line
                 strcat( str_temp, line); //concatenate to previous line
-                //printf("temp str=%s", str_temp); //test print - will remove later
             }
-            for(int i = 0;i < 80; i++) {
+            for(int i = 0;i < 80; i++) {//replaces any newlines with spaces
                 if(str_temp[i] == '\n') {
                     str_temp[i] = ' ';
                 }
             }
-            fprintf(cp, "%s", str_temp); //test print - will remove later
+            fprintf(cp, "%s", str_temp); //once done it writes to the file
         }
-        if(strcmp(");\n", line) == 0) {
+        if(strcmp(");\n", line) == 0) {//skips writing extra ending parentheses
             getline(&line, &len, op);
         }
+        //replaces josecode key words
         if(strcmp(josecode[0], line) == 0) {
-            fprintf(cp, "%s", native[0]);        
+            fprintf(cp, "%s", native[0]); 
         } else if(strcmp(josecode[1], line) == 0) {
             fprintf(cp, "%s", native[1]);
         } else if(strcmp(josecode[2], line) == 0) {
@@ -112,16 +110,18 @@ int main(void){
         } 
     }
 
-    while(*j != "") {
+    //Josh
+    while(*j != "") {//writes to end of file
         fprintf(cp, "%s\n", *j++);
     }
-
+    //closes all files and frees line for memory purposes
     fclose(op);
     fclose(fp);
     fclose(cp);
     free(line);
-    printf("Finished Compiling josecode\n");
+    printf("Finished Compiling josecode\n");//success message
     
-    system("cd ../output && make run");
+    //Matt
+    system("cd ../output && make run");//runs output.c file
     return(SUCCESS);
 }
